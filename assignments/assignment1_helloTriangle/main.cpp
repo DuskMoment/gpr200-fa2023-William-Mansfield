af11c6a2406f77b71c6905d9e7e6126e8f0951b4
Will <william.mansfield@mymail.champlain.edu>
2023-09-01 11:26:21 -0400

    First commit

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -34,4 +34,53 @@ int main() {
 		glfwSwapBuffers(window);
 	}
 	printf("Shutting down...");
+
+	//defining vertex postions
+
+	float vertices[9] =
+	{
+		// all Z's zero becuase it is a 2d object
+		//x //y //z
+		-0.5, -0.5, 0.0, //bottem left
+		0.5, -0.5, 0.0, // bottem right
+		0.0, 0.5, 0.0 // top left
+
+	};
+
+
+	//Vertex Buffer Object (VBO) (used to send data to the GPU a buffer)
+	//define new buffer id
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	//after creating our new vbo buffer we then put it in the GL_ARRAY_BUFFER slot making it our new buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	//allacate space to the buffer and send vertex data to the cpu
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//GL_STATIC_DRAW gives the compiler a hint that this buffer is for drawing
+	//if you wanted to change the contence of the buffer at run time you would use GL_DYNAMIC_DRAW.
+
+
+
+	//move to a fucntion
+	//Vertex Array Object (VAO)
+	unsigned int vao;
+	//why does this need more arguemnts?
+	glGenVertexArrays(vao);
+
+	glBindVertexArray(vao);
+	//tell vao to pull data from vbo (what is vbo in the contex of this function?)
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	//define position atributes
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)0);
+	//next step is tor refactor in to a function with a handle(what does this mean?) to a newley created VAO
+
+	
+
 }
+
+unsigned int createVAO(float* vertexData, int numVertices)
+{
+	
+
+};
