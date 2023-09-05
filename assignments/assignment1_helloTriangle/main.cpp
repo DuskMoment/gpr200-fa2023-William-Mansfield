0edda162e71355578d8e0e4ada7c46d82d9072b5
Will <william.mansfield@mymail.champlain.edu>
2023-09-05 19:32:22 -0400

    Completed up until mulitple vertex attributes

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -9,13 +9,13 @@ const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
 //this will make our triangle
-float vertices[9] =
+float vertices[21] =
 {
 	// all Z's zero becuase it is a 2d object
-	//x //y //z
-	-0.5, -0.5, 0.0, //bottem left
-	0.5, -0.5, 0.0, // bottem right
-	0.0, 0.5, 0.0 // top left
+	//x   //y   //z   //r  //g  //b // a
+	-0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 1.0,//bottem left
+	0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 1.0, // bottem right
+	0.0, 0.5, 0.0, 0.0, 0.0, 1.0, 1.0 // top left
 
 };
 //vertex shader glsl is the shader language that is why it is in quotes
@@ -24,6 +24,7 @@ const char* vertexShaderSource = R"(
 	#version 450
 	layout(location = 0) in vec3 vPos;
 	void main(){
+		
 		gl_Position = vec4(vPos,1.0);
 	}
 )";
@@ -38,6 +39,88 @@ const char* fragmentShaderSource = R"(
 		FragColor = vec4(1.0);
 	}
 )";
+unsigned int createVAO(float* vertexData, int numVertices)
+{
+
+	//needs to return vao
+	//Vertex Array Object (VAO)
+	unsigned int vao;
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	
+	
+
+	//Vertex Buffer Object (VBO) (used to send data to the GPU a buffer)
+	//define new buffer id
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	//after creating our new vbo buffer we then put it in the GL_ARRAY_BUFFER slot making it our new buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	//allacate space to the buffer and send vertex data to the cpu. it also sends data to our gpu
+	glBufferData(GL_ARRAY_BUFFER, (sizeof(float) * numVertices), vertexData, GL_STATIC_DRAW);
+	//GL_STATIC_DRAW gives the compiler a hint that this buffer is for drawing
+	//if you wanted to change the contence of the buffer at run time you would use GL_DYNAMIC_DRAW.
+
+	//define position atributes (tells us how to handel the float data from the vertices)
+	//position artibute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)0);
+	glEnableVertexAttribArray(0);
+
+	////color atribute
+	/*glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (const void*)(sizeof(float) * 3));
+	glEnableVertexAttribArray(1);*/
+	
+
+	return vao;
+};
+
+unsigned int createShader(GLenum shaderType, const char* sourceCode)
+{
+	unsigned int shader = glCreateShader(shaderType);
+
+	glShaderSource(shader, 1, &sourceCode, NULL);
+	glCompileShader(shader);
+
+	int success;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		char infoLog[512];
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		printf("Failed to compile shader: %s", infoLog);
+		return -1;
+	}
+	return shader;
+};
+
+unsigned int createShaderProgram(const char* vertexShaderSource, const char* fragmentShaderSource)
+{
+	int success;
+	unsigned int vertexShader = createShader(GL_VERTEX_SHADER, vertexShaderSource);
+
+	//attach each stage
+	unsigned int fragmentShader = createShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
+
+	unsigned int shaderProgram = glCreateProgram();
+	glLinkProgram(shaderProgram);
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		char infoLog[512];
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		printf("Failed to compile shader: %s", infoLog);
+		return -1;
+	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return shaderProgram;
+
+};
 
 int main() {
 	printf("Initializing...");
@@ -59,68 +142,10 @@ int main() {
 		return 1;
 	}
 	//finds all of the gl functions. all fuctions should start with gl -- commuitcating from the cpu to the gpu
-	//MKAE A FUNCTION
 	//Vertex Array Object (VAO)
-	unsigned int vao;
+	unsigned int vao = createVAO(vertices, 9);
 
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-
-
-	//next step is tor refactor in to a function with a handle(what does this mean?) to a newley created VAO
-
-	//Vertex Buffer Object (VBO) (used to send data to the GPU a buffer)
-	//define new buffer id
-	unsigned int vbo;
-	glGenBuffers(1, &vbo);
-	//after creating our new vbo buffer we then put it in the GL_ARRAY_BUFFER slot making it our new buffer
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	//allacate space to the buffer and send vertex data to the cpu. it also sends data to our gpu
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	//GL_STATIC_DRAW gives the compiler a hint that this buffer is for drawing
-	//if you wanted to change the contence of the buffer at run time you would use GL_DYNAMIC_DRAW.
-
-	//define position atributes (tells us how to handel the float data from the vertices
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)0);
-	glEnableVertexAttribArray(0);
-	//MAKE A FUNCTION
-
-	//MAKE ANOTHER FUNCTION this is the body for create shader
-	//create a new shader object
-	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-
-	int success;
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		char infoLog[512];
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		printf("Failed to compile shader: %s", infoLog);
-	}
-	//MAKE ANOTHER FUNCTION
-
-	unsigned int vertexShader = createShader(GL_VERTEX_SHADER, vertexShaderSource);
-
-	//attach each stage
-	unsigned int fragmentShader = createShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
-	unsigned int shaderProgram = glCreateProgram();
-	glLinkProgram(shaderProgram);
-
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		char infoLog[512];
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		printf("Failed to compile shader: %s", infoLog);
-	}
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-	//next step is render loop
-
+	unsigned int shader = createShaderProgram(vertexShaderSource, fragmentShaderSource);
 
 	//this is our render loop
 	while (!glfwWindowShouldClose(window)) {
@@ -130,17 +155,12 @@ int main() {
 
 		// draw calls go here
 		glBindVertexArray(vao);
+		glUseProgram(shader);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 	}
 	printf("Shutting down...");
 
-	
-
 }
 
-unsigned int createVAO(float* vertexData, int numVertices)
-{
-	
-	//needs to return vao
-};
