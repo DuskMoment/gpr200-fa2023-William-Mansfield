46e6c079ae1fa069f477eeb874b77741caae4bb7
Will <william.mansfield@mymail.champlain.edu>
2023-09-15 18:18:55 -0400

    created Vertex struct, UV mapped quad

--- assignments/assignment2_sunset/main.cpp
+++ assignments/assignment2_sunset/main.cpp
@@ -10,21 +10,31 @@
 
 #include <wm/shader.h>
 
+struct Vertex {
+	float x, y, z;
+	float u, v;
+};
 unsigned int createShader(GLenum shaderType, const char* sourceCode);
 unsigned int createShaderProgram(const char* vertexShaderSource, const char* fragmentShaderSource);
-unsigned int createVAO(float* vertexData, int numVertices);
+unsigned int createVAO(Vertex* vertexData, int numVertices, unsigned int* indicesData, int numIndices);
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
-float vertices[9] = {
-	//x   //y  //z   
-	-0.5, -0.5, 0.0, 
-	 0.5, -0.5, 0.0,
-	 0.0,  0.5, 0.0 
+Vertex vertices[4] = {
+	//x   //y  //z //u //v
+	{-1.0, -1.0, 0.0 , 0.0, 0.0}, // bottom L 0
+	{ 1.0,-1.0, 0.0, 1.0, 0.0}, // bottom R 1
+	{1.0, 1.0, 0.0, 1.0, 1.0}, //top R 2
+	{-1.0, 1.0, 0.0, 0.0, 1.0} //top L 3
+ 
 };
 
+unsigned int indices[6] = {
+	0,1,2,
+	0,2,3
+};
 //const char* vertexShaderSource = R"(
 //	#version 450
 //	layout(location = 0) in vec3 vPos;
@@ -83,8 +93,9 @@ int main() {
 	wm::Shader shader("assets/vertexShader.vert", "assets/fragmentShader.frag");
 	shader.use();
 
-	unsigned int vao = createVAO(vertices, 3);
-
+	//shouldent this be 6 and 2? but when that happens it does not work
+	unsigned int vao = createVAO(vertices, 4 ,indices, 6);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	/*glUseProgram(shader);*/
 	glBindVertexArray(vao);
 
@@ -98,10 +109,12 @@ int main() {
 		//glUniform1f(glGetUniformLocation(shader,"_Brightness"), triangleBrightness);
 
 		//NEW STUFF THAT I MADE
-		shader.setVec3("_Color", triangleColor[0], triangleColor[1], triangleColor[2]);
-		shader.setFloat("_Brightness", triangleBrightness);
+		//shader.setVec3("_Color", triangleColor[0], triangleColor[1], triangleColor[2]);
+		//shader.setFloat("_Brightness", triangleBrightness);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
+
 
 		//Render UI
 		{
@@ -171,7 +184,7 @@ unsigned int createShaderProgram(const char* vertexShaderSource, const char* fra
 	return shaderProgram;
 }
 
-unsigned int createVAO(float* vertexData, int numVertices) {
+unsigned int createVAO(Vertex* vertexData, int numVertices, unsigned int* indicesData, int numIndices) {
 	unsigned int vao;
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
@@ -181,11 +194,21 @@ unsigned int createVAO(float* vertexData, int numVertices) {
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	//Allocate space for + send vertex data to GPU.
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * numVertices * 3, vertexData, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * numVertices * 5, vertexData, GL_STATIC_DRAW);
+
+	unsigned int ebo;
+	glGenVertexArrays(1, &ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * numIndices, indicesData, GL_STATIC_DRAW);
 
 	//Position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*)0);
 	glEnableVertexAttribArray(0);
+
+	//UV
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*)(offsetof(Vertex, u)));
+	glEnableVertexAttribArray(1);
+
 
 	return vao;
 }
